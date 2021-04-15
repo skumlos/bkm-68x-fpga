2698ef4d4821a91ff734f1d5f2f6a4fcc9727629
Martin Hejnfelt <martin@hejnfelt.com>
2021-04-15 18:56:17 +0200

    Added initial line detection

--- video_format_detector.v
+++ video_format_detector.v
@@ -13,11 +13,18 @@ Clock_divider clk25mhz(
 );
 
 wire vsync_filter;
+wire hsync_filter;
 
 simplefilter vs_filter(
 	.clk_50mhz_in(clk_50mhz_in),
 	.sig_in(vsync_in),
 	.sig_out(vsync_filter)
+);
+
+simplefilter hs_filter(
+	.clk_50mhz_in(clk_50mhz_in),
+	.sig_in(hsync_in),
+	.sig_out(hsync_filter)
 );
 
 defparam clk25mhz.DIVISOR = 28'd2;
@@ -26,33 +33,26 @@ localparam frq_hz = 50000000;
 
 reg [31:0] hsync_cnt = 'h00;
 
-reg reset = 1'b0;
-reg has_reset = 1'b0;
-
 reg [31:0] vsync_frq = 'h0;
 
 reg [31:0] clk_count = 'h0;
 
 reg [7:0] reg_format = 'h00;
 
-reg interlaced = 1'b1;
-
 wire vsync_count_clk = clk & vsync_filter;
+wire hsync_count_clk = hsync_filter & vsync_filter;
 
 assign video_format = reg_format;
 
-assign sample_out = vsync_count_clk;
+assign sample_out = hsync_count_clk;
 
 initial begin
 	clk_count = 'h0;
-	reset = 1'b0;
-	has_reset = 1'b0;
 	
 	hsync_cnt = 'h0;
 	vsync_frq = 'h0;
 
 	reg_format = 'h00;
-	interlaced = 1'b1;
 end
 
 always @ (posedge clk_50mhz_in) begin
@@ -62,39 +62,47 @@ always @ (posedge clk_50mhz_in) begin
 	end
 end
 
+reg hs_firstin = 1'b1;
+always @ (negedge hsync_count_clk, negedge vsync_filter) begin
+	if(!vsync_filter) begin
+		hs_firstin <= 1'b1;
+	end else begin
+		if(hs_firstin) begin
+			hsync_cnt <= 1;
+		end else begin
+			hsync_cnt <= hsync_cnt + 1'b1;
+		end
+		hs_firstin <= 1'b0;
+	end
+end
+
 always @ (negedge vsync_filter) begin
 	if(vsync_frq == 50) begin
-		if(interlaced == 1'b1) begin
+		if(hsync_cnt < 380) begin
 			reg_format = 'h01; // 576i50
-		end else begin
+		end else if(hsync_cnt < 550) begin
+			reg_format = 'h0B; // 1080i50, unverified
+		end else if(hsync_cnt < 590) begin
 			reg_format = 'h03; // 576p50
+		end else if(hsync_cnt < 740) begin
+			reg_format = 'h12; // 720p50, unverified
+		end else begin
+			reg_format = 'h00; // sum-ting-wong
 		end
 	end else if(vsync_frq == 60) begin
-		if(interlaced == 1'b1) begin
+		if(hsync_cnt < 300) begin
 			reg_format = 'h02; // 480i60
-		end else begin
+		end else if(hsync_cnt < 500) begin
 			reg_format = 'h04; // 480p60
+		end else if(hsync_cnt < 560) begin
+			reg_format = 'h0C; // 1080i60
+		end else if(hsync_cnt < 740) begin
+			reg_format = 'h13; // 720p60
+		end else begin
+			reg_format = 'h00; // sum-ting-wong
 		end
 	end else begin
 		reg_format = 'h00;
-	end
-end
-
-/*
-always @ (negedge reset) begin
-	if(hsync_cnt > 17500) begin
-		interlaced <= 1'b0;
-	end else begin
-		interlaced <= 1'b1;
-	end
-end
-*/
-
-always @ (posedge hsync_in, posedge reset) begin
-	if(reset) begin
-		hsync_cnt <= 0;
-	end else begin
-		hsync_cnt <= hsync_cnt + 1'b1;
 	end
 end
 
@@ -111,31 +119,17 @@ always @ (negedge vsync_filter) begin
 		vsync_frq = 0;
 	end
 end
-/*
-always @ (negedge vsync_in) begin
-	if(clk_in_vsync_cnt < 70000) begin
-		vsync_frq = 0;
-	end else if(clk_in_vsync_cnt < 850000) begin
-		vsync_frq = 60;
-	end else if(clk_in_vsync_cnt < 1050000) begin
-		vsync_frq = 50;
-	end else begin
-		vsync_frq = 0;
-	end
-end
-*/
+
+
 reg firstin = 1'b1;
 always @ (negedge vsync_count_clk, negedge vsync_filter) begin
 	if(!vsync_filter) begin
 		firstin <= 1'b1;
-		reset <= 1;
 	end else begin
 		if(firstin) begin
 			clk_in_vsync_cnt <= 1;
-			reset <= 0;
 		end else begin
 			clk_in_vsync_cnt <= clk_in_vsync_cnt + 1'b1;
-			reset <= 0;
 		end
 		firstin <= 1'b0;
 	end
